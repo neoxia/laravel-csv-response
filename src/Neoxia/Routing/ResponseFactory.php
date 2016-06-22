735aa3e05d4b1fe48eb9aa9bfe0fea7c8d7e4e29
alexandre-butynski <alexandre.butynski@neoxia.com>
2016-06-22 13:15:01 +0200

    The cvs response can parse several types of data - Collection of objects - Associative or sequential array - String

--- src/Neoxia/Routing/ResponseFactory.php
+++ src/Neoxia/Routing/ResponseFactory.php
@@ -2,13 +2,15 @@
 
 namespace Neoxia\Routing;
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 use Illuminate\Routing\ResponseFactory as BaseResponseFactory;
 
 class ResponseFactory extends BaseResponseFactory
 {
     public function csv($data)
     {
-        if ($data->isEmpty()) {
+        if ($this->dataIsEmpty($data)) {
             return $this->make('No Content', 204);
         }
 
@@ -18,15 +20,52 @@ class ResponseFactory extends BaseResponseFactory
         return $this->make($csv, 200, $headers);
     }
 
+    protected function dataIsEmpty($data)
+    {
+        if ($data instanceof Collection) {
+            return $data->isEmpty();
+        } else {
+            return empty($data);
+        }
+    }
+
     protected function formatCsv($data)
     {
-        $csv = implode(';', array_keys($data->first()->csvSerialize()));
-
-        foreach ($data as $row) {
-            $csv .= "\r\n" . implode(';', $row->csvSerialize());
+        if (is_string($data)) {
+            return $data;
         }
 
-        return $csv;
+        $csvArray = [];
+
+        $this->addHeaderToCsvArray($csvArray, $data);
+        $this->addRowsToCsvArray($csvArray, $data);
+
+        return implode("\r\n", $csvArray);
+    }
+
+    protected function addHeaderToCsvArray(&$csvArray, $data)
+    {
+        $firstRowData = $this->getRowData($data[0]);
+
+        if (Arr::isAssoc($firstRowData)) {
+            $csvArray[0] = implode(';', array_keys($firstRowData));
+        }
+    }
+
+    protected function addRowsToCsvArray(&$csvArray, $data)
+    {
+        foreach ($data as $row) {
+            $csvArray[] = implode(';', $this->getRowData($row));
+        }
+    }
+
+    protected function getRowData($row)
+    {
+        if (is_object($row)) {
+            return $row->csvSerialize();
+        } else {
+            return $row;
+        }
     }
 
     protected function createCsvHeaders()
--- tests/ResponseFactoryTest.php
+++ tests/ResponseFactoryTest.php
@@ -16,7 +16,7 @@ class ResponseFactoryTest extends PHPUnit_Framework_TestCase
         $this->responseFactory = new ResponseFactory($viewFactory, $redirector);
     }
 
-    public function testCsvResponseWithEmptyDataReturnNoContent()
+    public function testCsvResponseWithEmptyCollectionReturnNoContent()
     {
         $data = new Collection;
 
@@ -26,14 +26,65 @@ class ResponseFactoryTest extends PHPUnit_Framework_TestCase
         $this->assertEquals("No Content", $response->getContent());
     }
 
-    public function testCsvResponseWithBasicData()
+    public function testCsvResponseWithEmptyArrayReturnNoContent()
+    {
+        $data = [];
+
+        $response = $this->responseFactory->csv($data);
+
+        $this->assertEquals(204, $response->getStatusCode());
+        $this->assertEquals("No Content", $response->getContent());
+    }
+
+    public function testCsvResponseWithEmptyStringReturnNoContent()
+    {
+        $data = '';
+
+        $response = $this->responseFactory->csv($data);
+
+        $this->assertEquals(204, $response->getStatusCode());
+        $this->assertEquals("No Content", $response->getContent());
+    }
+
+    public function testCsvResponseWithCollectionData()
     {
         $data = new Collection([new ModelStub(['first_name' => 'John', 'last_name' => 'Doe'])]);
 
         $response = $this->responseFactory->csv($data);
 
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals("first_name;last_name\r\nJohn;Doe", $response->getContent());
+        $this->assertCsvResponseIsValidAndEquals($response, "first_name;last_name\r\nJohn;Doe");
+    }
+
+    public function testCsvResponseWithSequentialArrayData()
+    {
+        $data = [['first_name', 'last_name'], ['John', 'Doe']];
+
+        $response = $this->responseFactory->csv($data);
+
+        $this->assertCsvResponseIsValidAndEquals($response, "first_name;last_name\r\nJohn;Doe");
+    }
+
+    public function testCsvResponseWithAssociativeArrayData()
+    {
+        $data = [['first_name' => 'John', 'last_name' => 'Doe']];
+
+        $response = $this->responseFactory->csv($data);
+
+        $this->assertCsvResponseIsValidAndEquals($response, "first_name;last_name\r\nJohn;Doe");
+    }
+
+    public function testCsvResponseWithStringData()
+    {
+        $data = "first_name;last_name\r\nJohn;Doe";
+
+        $response = $this->responseFactory->csv($data);
+
+        $this->assertCsvResponseIsValidAndEquals($response, "first_name;last_name\r\nJohn;Doe");
+    }
+
+    protected function assertCsvResponseIsValidAndEquals($response, $csvBody)
+    {
+        $this->assertEquals($csvBody, $response->getContent());
         $this->assertEquals('text/csv; charset=WINDOWS-1252', $response->headers->get('Content-Type'));
         $this->assertEquals('WINDOWS-1252', $response->headers->get('Content-Encoding'));
         $this->assertEquals('binary', $response->headers->get('Content-Transfer-Encoding'));
